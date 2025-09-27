19ff0e40258344dfe7298c95d167ac9c1f2133b5
y-minion <dudals2963@tukorea.ac.kr>
2025-09-27 14:00:55 +0900

    ✨ 클라이언트 컴포넌트에 삭제 기능 구현
    서버액션을 리액트 쿼리의 mutationFn을 통해 사용한다. 이를 통해 파일의 삭제 기능을 구현완료

--- supabase-dropbox-clone/components/dropbox-image.tsx
+++ supabase-dropbox-clone/components/dropbox-image.tsx
@@ -1,9 +1,23 @@
 "use client";
 
-import { IconButton } from "@material-tailwind/react";
+import { IconButton, Spinner } from "@material-tailwind/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteFile } from "actions/storageActions";
 import { getImageUrl } from "utils/supabase/storage";
 
 export default function DropboxImage({ image }) {
+  const queryClient = useQueryClient();
+  const deleteFileMutation = useMutation({
+    mutationFn: (fileName: string) => deleteFile(fileName),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["images"],
+        exact: false,
+      });
+    },
+  });
+
   return (
     <div className="relative w-full flex flex-col gap-2 p-4 border border-gray-100 rounded-xl shadow-md">
       {/* Image */}
@@ -18,8 +32,17 @@ export default function DropboxImage({ image }) {
       <div>{image.name}</div>
 
       <div className="absolute top-4 right-4 ">
-        <IconButton color="red">
-          <i className="fas fa-trash" />
+        <IconButton
+          color="red"
+          onClick={() => {
+            deleteFileMutation.mutate(image.name);
+          }}
+        >
+          {deleteFileMutation.isPending ? (
+            <Spinner />
+          ) : (
+            <i className="fas fa-trash" />
+          )}
         </IconButton>
       </div>
     </div>
